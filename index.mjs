4d2eff00e0f049f5961b25f4d0ddbaafc8b619f7
Gustavo Torres <gustavotorresds@gmail.com>
2023-09-10 14:52:30 -0700

    Remove some of the archived/commented code

--- index.mjs
+++ index.mjs
@@ -25,32 +25,3 @@ app.use((err, _req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
-
-// const cors = require('cors');
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const mongoString = process.env.DATABASE_URL;
-
-// dotenv.config();
-
-// mongoose.connect(mongoString);
-// const database = mongoose.connection;
-
-// database.on('error', (error) => {
-//   console.log(error)
-// })
-
-// database.once('connected', () => {
-//   console.log('Database Connected');
-// })
-
-// const app = express();  //Create new instance
-// app.use(cors())
-// app.use(express.json());
-
-// const routes = require('./routes/routes');
-// app.use('/api', routes);
-
-// app.listen(3000, () => {
-//     console.log(`Server Started at ${3000}`)
-// })
--- routes/trips.mjs
+++ routes/trips.mjs
@@ -4,7 +4,7 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-// Get a list of 50 posts
+// Get all trips
 router.get("/", async (req, res) => {
   let collection = await db.collection("trips");
   let results = await collection.find({})
@@ -14,10 +14,6 @@ router.get("/", async (req, res) => {
 });
 
 export default router;
-
-// const express = require('express');
-// const Model = require('../models/model');
-// const router = express.Router();
 
 // const got = require('got');
 
@@ -44,69 +40,3 @@ export default router;
 //     res.status(400).json({ message: error.message});
 //   }
 // })
-
-// //Post Method
-// router.post('/post', async (req, res) => {
-//   const data = new Model({
-//     name: req.body.name,
-//     age: req.body.age
-//   })
-
-//   try {
-//     const dataToSave = await data.save();
-//     res.status(200).json(dataToSave)
-//   }
-//   catch (error) {
-//     res.status(400).json({ message: error.message })
-//   }
-// })
-
-// //Get all Method
-// router.get('/getAll', async (req, res) => {
-//   try {
-//     const data = await Model.find();
-//     res.json(data);
-//   } catch (error) {
-//     res.status(500).json({message: error.message})
-//   }
-// })
-
-// //Get by ID Method
-// router.get('/getOne/:id', async (req, res) => {
-//   try {
-//     const data = await Model.findById(req.params.id);
-//     res.json(data);
-//   } catch(error) {
-//     res.status(500).json({message: error.message})
-//   }
-// })
-
-// //Update by ID Method
-// router.patch('/update/:id', async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const updatedData = req.body;
-//     const options = {new: true};
-
-//     const result = await Model.findByIdAndUpdate(
-//       id, updatedData, options
-//     );
-
-//     res.send(result);
-//   } catch (error) {
-//     res.status(400).json({ message: error.message})
-//   }
-// })
-
-// //Delete by ID Method
-// router.delete('/delete/:id', async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const data = await Model.findByIdAndDelete(id);
-//     res.send(`Document with ${data.name} has been deleted..`)
-//   } catch (error) {
-//     res.status(400).json({ message: error.message })
-//   }
-// })
-
-// module.exports = router;
