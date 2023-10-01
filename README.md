5a4b9724788cdad6a38f33ec78a1db720a5249dd
Gustavo Torres <gustavotorresds@gmail.com>
2023-10-01 15:32:43 -0700

    Pull info about best flight option ffor trip

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Trip planner server
 Server for Trip Planner app
+
+## Flight suggestions:
+Provide origin + destinations cities, get back a list of flights that match the closest airports
+* Uses SkyScanner to get the closest IATA codes (aiports)
+* Then pings SkyScanner for the best flights
+* Returns a list of options, in order of what we think is best
--- routes/trips.mjs
+++ routes/trips.mjs
@@ -1,10 +1,186 @@
 import express from "express";
 import db from "../db/conn.mjs";
 import { ObjectId } from "mongodb";
+import axios from "axios";
+import dayjs from 'dayjs';
 
 const router = express.Router();
 
-const generateItinerary = async (destinations) => {
+const skyScannerKey = process.env.SKYSCANNER_KEY || "";
+const SKYSCANNER_BASE_URL = 'https://partners.api.skyscanner.net/apiservices/v3/';
+const SKYSCANNER_ROUTE_AUTOSUGGEST = 'autosuggest/flights';
+const SKYSCANNER_ROUTE_FLIGHTSEARCH = 'flights/live/search/create';
+const SKYSCANNER_ROUTE_FLIGHTPOLL = 'flights/live/search/poll';
+
+const skyScannerAxios = axios.create({
+  baseURL: SKYSCANNER_BASE_URL,
+  timeout: 10000,
+  headers: {
+    'x-api-key': skyScannerKey,
+    'Content-Type': 'application/json',
+  },
+});
+
+const getIataCode = async (city) => {
+  // IMPORTANT TODO: create own dictionary of city -> IATA code that we can retrieve from. When not found, can ping SkyScanner, and save result.
+  // Create a friendly way to interact with origin + all destinations and dates (depends on APIs)
+  // TODO: prob want to use entityID instead
+  const response = await skyScannerAxios.post(SKYSCANNER_ROUTE_AUTOSUGGEST, {
+    query: {
+      market: 'US',
+      locale: 'en-US',
+      searchTerm: city,
+      includedEntityTypes: ['PLACE_TYPE_CITY', 'PLACE_TYPE_AIRPORT'],
+      limit: 5,
+      isDestination: true, // TODO: should it be false?
+    },
+  });
+
+  return response.data.places[0].iataCode;
+}
+
+const getPlaceFromId = async (entityId) => {
+  let placesCollection = db.collection("flight_places");
+
+  const query = { [`places.${entityId}`]: { $exists: true } };
+  const projection = { projection: { [`places.${entityId}`]: 1, _id: 0 } };
+
+  // Find the document with specified ID
+  const result = await placesCollection.findOne(query, projection);
+
+  console.log('PLACE NAME: ', result);
+
+  if(!!result) {
+    return result.places[`${entityId}`];
+  }
+
+  return undefined;
+}
+
+const getFlightItinerary = async (startDateJS, cityFrom, destinations) => {
+  // Find the IATA code relative to origin + all destinations
+  const { cityFromIataCode, destinationsIataCodes } =  {
+    cityFromIataCode: 'SAO',
+    destinationsIataCodes: ['PAR', 'LON'],
+  };
+  // const cityFromIataCode = await getIataCode(cityFrom);
+
+  // const destinationsIataCodes = [];
+  // for (let i = 0; i < destinations.length; i++) {
+  //   const destinationIataCode = await getIataCode(destinations[i].city)
+  //   destinationsIataCodes.push(destinationIataCode);
+  // }
+
+  const queryLegs = [];
+
+  let latestDateJS = startDateJS;
+
+  for (let i = 0; i < destinations.length + 1; i++) {
+    const isFirst = (i <= 0);
+    const isLast = (i >= destinations.length);
+
+    const queryLeg = {
+      originPlaceId: {
+        iata: (isFirst ? cityFromIataCode : destinationsIataCodes[i - 1]),
+      },
+      destinationPlaceId: {
+        iata: (isLast ? cityFromIataCode : destinationsIataCodes[i]),
+      },
+      date: {
+        year: latestDateJS.year(),
+        month: latestDateJS.month() + 1, // 0-indexed
+        day: latestDateJS.date(),
+      }
+    }
+
+    queryLegs.push(queryLeg);
+    if (!isLast) {
+      latestDateJS = latestDateJS.add(destinations[i].numberOfDays, 'day');
+    }
+  }
+
+  console.log('LEGS: ', queryLegs);
+
+  const itineraryResposne = await skyScannerAxios.post(SKYSCANNER_ROUTE_FLIGHTSEARCH, {
+    query: {
+      market: 'US',
+      locale: 'en-US',
+      currency: 'USD',
+      query_legs: queryLegs,
+      adults: 1,
+      childrenAges: [],
+      cabinClass: 'CABIN_CLASS_ECONOMY',
+      excludedAgentsIds: [],
+      excludedCarriersIds: [],
+      includedAgentsIds: [],
+      includedCarriersIds: [],
+      nearbyAirports: false,
+    }
+  });
+
+  const sessionToken = itineraryResposne.data.sessionToken;
+
+  const itineraryPollResponse = await skyScannerAxios.post(`${SKYSCANNER_ROUTE_FLIGHTPOLL}/${sessionToken}`, {
+    query: {
+      market: 'US',
+      locale: 'en-US',
+      currency: 'USD',
+      query_legs: queryLegs,
+      adults: 1,
+      childrenAges: [],
+      cabinClass: 'CABIN_CLASS_ECONOMY',
+      excludedAgentsIds: [],
+      excludedCarriersIds: [],
+      includedAgentsIds: [],
+      includedCarriersIds: [],
+      nearbyAirports: false,
+    }
+  });
+
+  // console.log('TOKEN: ', sessionToken);
+  // console.log('ITINERARIES: ', itineraryPollResponse.data.content.results.itineraries);
+
+  const suggestedItineraryId = itineraryPollResponse.data.content.sortingOptions.best[0].itineraryId;
+  const suggestedItinerary = itineraryPollResponse.data.content.results.itineraries[suggestedItineraryId];
+  const suggestedLegs = [];
+  for (let i = 0; i < suggestedItinerary.legIds.length; i++) {
+    const legId = suggestedItinerary.legIds[i];
+    const legObj = itineraryPollResponse.data.content.results.legs[legId];
+
+    const originInfo = await getPlaceFromId(legObj.originPlaceId);
+    const destinationInfo = await getPlaceFromId(legObj.destinationPlaceId);
+
+    legObj.originName = originInfo.name;
+    legObj.originIata = originInfo.iata;
+
+    legObj.destinationName = destinationInfo.name;
+    legObj.destinationIata = destinationInfo.iata;
+
+    suggestedLegs.push(legObj);
+
+    // const carriers = legObj.operatingCarrierIds;
+    // for (let j = j < carriers.length; j++) {
+    //   const carrier =
+    // }
+  }
+
+  suggestedItinerary.legs = suggestedLegs;
+
+  return suggestedItinerary;
+}
+
+const generateItinerary = async (startDateJS, cityFrom, destinations) => {
+  // Request the itinerary for all those IATA codes and dates
+  // TODO: remove comment
+
+  const suggestedItinerary =  await getFlightItinerary(startDateJS, cityFrom, destinations);
+  console.log('ITINERARY: ', suggestedItinerary)
+
+
+  // Maybe: convert response to a more friendly format
+  // [...]
+
+  // Find activity options
   let destinationsCollection = await db.collection("destinations");
 
   // Build empty itinerary
@@ -12,6 +188,8 @@ const generateItinerary = async (destinations) => {
 
   // Update the trip itinerary: for each destination, dep on the number of days you are there, find a reasonable number of activities to do, spread it across the days
   for (let destinationIdx = 0; destinationIdx < destinations.length; destinationIdx++) {
+    // TODO: Add flight to destination
+
     // For each of the destinations, find the activities to do
     const destination = destinations[destinationIdx];
     const dbDestination = await destinationsCollection.findOne({ name: destination.city })
@@ -64,13 +242,15 @@ router.patch('/:id', async (req, res) => {
 
   const { cityFrom, startDate, destinations, } = req.body;
 
+  const startDateJS = dayjs(startDate);
+
   try {
     const updates = {
       $set: {
           startDate: new Date(startDate),
           cityFrom: cityFrom,
           destinations: destinations,
-          itinerary: await generateItinerary(destinations),
+          itinerary: await generateItinerary(startDateJS, cityFrom, destinations),
         }
       };
 
